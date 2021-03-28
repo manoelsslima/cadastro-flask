8b57b629b3c1daaa7266a17f4634149f010ad34f
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-03-27 19:14:11 -0500

    List of customers implemented

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+class Customer:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+customer_list = []
+
+c1 = Customer('John', '+55 (11) 3026-7874', 'john.venn@gmail.com')
+c2 = Customer('Marie', '+55 (68) 3227-9999', 'marie.doss@gmail.com')
+
+customer_list.append(c1)
+customer_list.append(c2)
+
+@app.route('/list')
+def index():
+    return render_template('customer-list.html', title='Customer list', customers = customer_list)
 
 app.run(debug=True)
