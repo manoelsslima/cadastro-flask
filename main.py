fb0adfdfc129ea61c71ebd6fe3c8130da80f77ac
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-03-28 04:32:01 -0500

    Configurations for the database

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request, redirect
+from dao import CustomerDAO
+from flask_mysqldb import MySQL
+from models import Customer
 
 app = Flask(__name__)
 
-# todo - protect the attributes
-class Customer:
-    def __init__(self, name, phone, email):
-        self.name = name
-        self.phone = phone
-        self.email = email
+# database configurations
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'flask_user'
+app.config['MYSQL_PASSWORD'] = 'flask_Password123'
+app.config['MYSQL_DB'] = 'cadastro'
+app.config['MYSQL_PORT'] = 3306
+db = MySQL(app) # setting configurations
+# IoC
+customer_dao = CustomerDAO(db)
 
 customer_list = []
 
@@ -34,7 +40,9 @@ def create_customer():
     phone = request.form['phone']
     email = request.form['email']
     customer = Customer(name, phone, email)
-    customer_list.append(customer)
+    #customer_list.append(customer)
+    customer_dao.save(customer)
+
     return redirect('/list')
 
 app.run(debug=True)
