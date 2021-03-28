7159040ba421328ea6c1c5c8ec09114bb4357400
Manoel de Souza Silva Lima <manoel.lima@tjac.jus.br>
2021-03-27 19:36:57 -0500

    Add customer implemented

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# todo - protect the attributes
 class Customer:
     def __init__(self, name, phone, email):
         self.name = name
@@ -10,14 +11,30 @@ class Customer:
 
 customer_list = []
 
+# todo - remove the hard coded customers
 c1 = Customer('John', '+55 (11) 3026-7874', 'john.venn@gmail.com')
 c2 = Customer('Marie', '+55 (68) 3227-9999', 'marie.doss@gmail.com')
 
+# todo - remove the hard coded customers
 customer_list.append(c1)
 customer_list.append(c2)
 
 @app.route('/list')
 def index():
+    return render_template('customer-list.html', title='Customer list', customers = customer_list)
+
+@app.route('/new')
+def new_customer():
+    return render_template('customer-new.html', title='Add new customer')
+
+# GET is the default method. We need POST.
+@app.route('/create', methods=['POST',])
+def create_customer():
+    name = request.form['name']
+    phone = request.form['phone']
+    email = request.form['email']
+    customer = Customer(name, phone, email)
+    customer_list.append(customer)
     return render_template('customer-list.html', title='Customer list', customers = customer_list)
 
 app.run(debug=True)
